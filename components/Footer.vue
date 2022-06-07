9e961a3c185391471da018ef875123cc3d589cf4
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-07 22:59:01 +0100

    footer padding update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-span-4 sm:col-span-12 flex justify-center items-center mb-12">
+  <div class="col-span-4 sm:col-span-12 flex justify-center items-center pb-12">
     <p class="text-gray-500 text-sm font-sans">&copy; Copyright 2022 Shevon Salmon</p>
   </div>
 </template>
