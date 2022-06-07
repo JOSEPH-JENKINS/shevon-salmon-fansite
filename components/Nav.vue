e6ae2f291e97e6f201836c385ac8df4e65f2cfcd
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-07 13:41:30 +0100

    blend mode change 3

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="z-50 fixed w-screen h-20 px-6 top-0 left-0">
-    <div class="relative w-full h-full">
-      <div class="logo absolute top-1/2 -translate-y-1/2">
-        <span class="bg-white">
+  <div class="z-50 fixed w-screen h-20 px-6 top-0 left-0 bg-transparent">
+    <div class="relative w-full h-full bg-transparent">
+      <div class="logo absolute top-1/2 -translate-y-1/2 bg-transparent">
+        <span class="bg-transparent">
           <h1 class="font-bold text-white md:text-base uppercase mix-blend-difference">shevon salmon</h1>
         </span>
       </div>
