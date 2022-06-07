0e89bcb1a395dcbb59f215c0ea856fcf5ef798ab
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-07 22:12:57 +0100

    p to div

--- pages/index.vue
+++ pages/index.vue
@@ -26,9 +26,9 @@
       <img src="~/assets/img/shevon-hero.jpg" class="w-full h-full object-cover" alt="">
     </section>
     <section class="sm:col-span-12 h-56 flex justify-center items-center col-span-4">
-      <p class="font-sans text-base">
+      <div class="font-sans text-base">
         So what's up y'all, It's your boy Shevon and guess what? I got a <span><h1 class="font-bold text-pink-500">Gumroad</h1></span> store. Go check out my icons and wallpapers.
-      </p>
+      </div>
     </section>
   </main>
 </template>
