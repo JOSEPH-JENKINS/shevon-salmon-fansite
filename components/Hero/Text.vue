bcf94dbd488972f0d6b23a56c328253aedfc44f6
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-07 22:38:48 +0100

    text hero update 2

--- components/Hero/Text.vue
+++ components/Hero/Text.vue
@@ -3,8 +3,6 @@
     <h1 class="font-bold md:text-[9rem] lg:text-[11rem] text-6xl uppercase leading-[.875]">
       <span>Digital</span>
       <span>Creator</span>
-      <span>200K</span><br>
-      <span>Subscribers</span>
     </h1>
   </div>
 </template>
