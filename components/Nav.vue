f7b0d843414cb380d887525593d2e9546e8e8260
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-09 16:32:32 +0100

    added menu to the navigation

--- components/Nav.vue
+++ components/Nav.vue
@@ -6,6 +6,11 @@
           <h1 class="font-bold text-white md:text-base uppercase mix-blend-difference">shevon salmon</h1>
         </span>
       </div>
+      <div class="menu absolute top-1/2 right-0 -translate-y-1/2 bg-transparent">
+        <span class="bg-transparent">
+          <h1 class="font-bold text-white md:text-base uppercase mix-blend-difference">menu</h1>
+        </span>
+      </div>
     </div>
   </div>
 </template>
