7a5aabeded1ca19f3965e861f546345976adebc6
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-09 16:35:28 +0100

    added more opimization to the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="col-span-4 sm:col-span-12 flex justify-center items-center pb-12">
-    <p class="text-gray-500 text-sm font-sans">&copy; Copyright 2022 Shevon Salmon</p><br />
+  <div class="col-span-4 sm:col-span-12 flex justify-between items-center pb-12">
+    <p class="text-gray-500 text-sm font-sans">&copy; Copyright 2022 Shevon Salmon</p>
     <p class="text-gray-500 text-sm font-sans">Developed by <a href="https://amjosephjenkins.netlify.com">Joseph Jenkins</a></p>
   </div>
 </template>
