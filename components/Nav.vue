07e12f52f8e04ec1172d7a4888fcfc9af8d86c56
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-10 22:47:40 +0100

    margin correction

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="z-50 fixed w-full left-0 h-20 pl-6 pr-12 top-0 bg-transparent">
+  <div class="z-50 fixed w-full left-0 h-20 pl-6 pr-10 md:pr-12 top-0 bg-transparent">
     <div class="relative w-full h-full bg-transparent">
       <div class="logo absolute top-1/2 -translate-y-1/2 bg-transparent">
         <span class="bg-transparent">
