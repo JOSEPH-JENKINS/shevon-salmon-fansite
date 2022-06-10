0527d50418db1d77279af081413224e15ad0b448
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-06-10 22:49:35 +0100

    margin correction

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="z-50 fixed w-full left-0 h-20 pl-6 pr-10 md:pr-12 top-0 bg-transparent">
+  <div class="z-50 fixed w-full left-0 h-20 pl-6 pr-6 md:pr-12 top-0 bg-transparent">
     <div class="relative w-full h-full bg-transparent">
       <div class="logo absolute top-1/2 -translate-y-1/2 bg-transparent">
         <span class="bg-transparent">
-          <h1 class="font-bold text-white md:text-base uppercase mix-blend-difference">shevon salmon</h1>
+          <h1 class="font-bold text-white md:text-base text-sm uppercase mix-blend-difference">shevon salmon</h1>
         </span>
       </div>
       <div class="menu absolute top-1/2 right-0 -translate-y-1/2 bg-transparent">
         <span class="bg-transparent">
-          <h1 class="font-bold text-white md:text-base uppercase mix-blend-difference">
+          <h1 class="font-bold text-white md:text-base text-sm uppercase mix-blend-difference">
             <span><a href="http://instagram.com/shevonsalmon_">INSTA</a></span>,
             <span><a href="http://twitter.com/its_shevi">TWitter</a></span>
           </h1>
